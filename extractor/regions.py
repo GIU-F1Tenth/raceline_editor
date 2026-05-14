791d6fb5b220fe751e7f40d8ddbebfda8f7fb2e7
FamALouiz <fam@awadlouis.com>
2026-05-14 16:43:49 +0200

    feat: add curvature region

--- extractor/regions.py
+++ extractor/regions.py
@@ -54,7 +54,33 @@ class OvertakingAllowedRegion(Region):
         )
 
 
+@dataclass
+class CurvatureRegion(Region):
+    REGION_TYPE: ClassVar[str] = "Curvature Region"
+
+    is_curved: bool = field(default=True, kw_only=True)
+
+    def to_dict(self):
+        return {
+            "type": self.REGION_TYPE,
+            "name": self.name,
+            "start": self.start_index,
+            "end": self.end_index,
+            "is_curved": bool(self.is_curved),
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return CurvatureRegion(
+            start_index=int(data["start"]),
+            end_index=int(data["end"]),
+            name=str(data.get("name", "")),
+            is_curved=bool(data.get("is_curved", True)),
+        )
+
+
 REGION_TYPES = {
     ConstantSpeedMultiplierRegion.REGION_TYPE: ConstantSpeedMultiplierRegion,
     OvertakingAllowedRegion.REGION_TYPE: OvertakingAllowedRegion,
+    CurvatureRegion.REGION_TYPE: CurvatureRegion,
 }
